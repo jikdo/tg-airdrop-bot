700205a49f4fa4c485d91775ec0d16b1a267e7a6
Kofi <expl00ra@protonmail.com>
2018-07-18 23:45:21 +0100

    defined new db funtions to check in the start function

--- db.py
+++ db.py
@@ -48,12 +48,27 @@ else:
         return conn
 
 
+def close_db_connection(conn, cursor):
+    """
+    Closes db connection
+
+    Args:
+        conn (obj): Connection object
+        cursor (obj): Cursor object
+
+    Returns:
+        -
+    """
+    conn.close()
+    cursor.close()
+
+
 # Create bounty table
 def create_table(connect_db):
     """
     Create sql table to host bounty information
 
-    params:
+    Args:
     connect_db - connect db function
     """
     conn = connect_db()
@@ -65,18 +80,21 @@ def create_table(connect_db):
         date_joined VARCHAR(255) NOT NULL,
         telegram_id INTEGER NOT NULL,
         chat_id INTEGER NOT NULL,
-        ref_code VARCHAR(255) NOT NULL,
-        eth_address VARCHAR(255) NOT NULL,
+        referral_code VARCHAR(255) NOT NULL,
+        wallet_address VARCHAR(255) NOT NULL,
         telegram_username VARCHAR(255) NOT NULL,
         twitter_username VARCHAR(255) NOT NULL,
-        facebook_name VARCHAR(255) NOT NULL,
-        gains INTEGER NOT NULL,
+        facebook_profile_link VARCHAR(355) NOT NULL,
+        telegram_channel_reward INTEGER NOT NULL,
+        telegram_group_reward INTEGER NOT NULL,
+        twitter_reward INTEGER NOT NULL,
+        facebook_reward INTEGER NOT NULL,
+        referral_reward INTEGER NOT NULL,
         referred_no INTEGER NOT NULL
     )
     """)
-    cursor.close()
     conn.commit()
-    conn.close()
+    close_db_connection(conn, cursor)
 
 
 def add_youtube_column(connect_db):
@@ -97,3 +115,86 @@ def add_youtube_column(connect_db):
     cursor.close()
     conn.commit()
     conn.close()
+
+
+
+def get_user_rewards(connect_db, telegram_id):
+    """
+    Get rewards of a user
+
+    Args:
+        connect_db (func): connect_db function
+        telegram_id (str): Telegram ID of user
+
+    Returns:
+        tuple: (telegram_channel, telegram_group, twitter, facebook, referrals, total)
+
+        or
+
+        None: if no user reward was found
+
+    Raises:
+        pyscopg2.Error: When there is error with dealing with the database
+    """
+
+    # get rewards
+    try:
+        # connect to database
+        conn, cursor = connect_db()
+
+        cursor.execute("""
+        SELECT telegram_channel, telegram_group, twitter, facebook, referrals)
+        FROM participants
+        WHERE telegram_id=%s
+        """,
+        (telegram_id,)
+        )
+
+
+        if cursor.fetchone()[0]:
+            telegram_channel, telegram_group, twitter, facebook, referrals = cursor.fetchone()[0]
+            total = telegram_channel + telegram_group + twitter + facebook + referrals
+            
+            close_db_connection(conn, cursor)
+            return (telegram_channel, telegram_group, twitter, facebook, referrals, total)
+        else:
+            close_db_connection(conn, cursor)
+            return None
+    except psycopg2.Error as e:
+        print(e.pgerror)
+
+
+def is_participant(connect_db, telegram_id):
+    """
+    Checks if user is already a participant
+
+    Args:
+        telegram_id (int): Telegram ID of participant
+
+    Returns:
+        bool: Returns True if user is a participant else False
+    """
+
+    try:
+        conn, cursor = connect_db()
+        cursor.execute("""
+        SELECT * FROM participants WHERE telegram_id=%s
+        """,
+        (telegram_id,))
+
+        participant = cursor.fetchone()
+
+        if participant:
+            return True
+        else:
+            return False
+    except psycopg2.Error as e:
+        print(e.pgerror)
+
+
+def get_total_rewards(connect_db):
+    """
+    
+
+def add_new_participant(connect_db, **kargs):
+    pass
