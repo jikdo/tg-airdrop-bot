b04d201e46f925a91556429db6bfab542ec3bf16
Kofi <expl00ra@protonmail.com>
2018-07-20 13:17:22 +0100

    added tasks module and separeted tasks elements from menu

--- menu.py
+++ menu.py
@@ -22,6 +22,9 @@ from db import (
     get_user_referred_no,
     get_user_referral_code
 )
+from tasks import (
+    tasks_markup,
+)
 
 # get config
 with open(r'config.json', 'r') as file:
@@ -60,51 +63,6 @@ def menu_relayer(bot, update):
 def send_task_list(bot, update):
     """ show tasks to complete """
     print("called")
-
-    telegram_channel_button = [
-        InlineKeyboardButton(
-            "📢 Join our news channel",
-            url="{}".format(config['social']['telegram_channel']),
-        ),
-    ]
-
-    telegram_group_button = [
-        InlineKeyboardButton(
-            "👫 Join our community",
-            url="{}".format(config['social']['telegram_group']),
-        ),
-    ]
-
-    twitter_button = [
-        InlineKeyboardButton(
-            "🐥 Twitter Bounty",
-            callback_data='twitter',
-        ),
-    ]
-
-    facebook_button = [
-        InlineKeyboardButton(
-            "📘 Facebook Bounty",
-            callback_data='facebook',
-        ),
-    ]
-
-    youtube_button = [
-        InlineKeyboardButton(
-            "📺 Youtube Bounty",
-            callback_data='youtube'
-        )
-    ]
-
-    task_list_buttons = [
-        telegram_channel_button,
-        telegram_group_button,
-        twitter_button,
-        facebook_button,
-        youtube_button,
-    ]
-
-    tasks_markup = InlineKeyboardMarkup(task_list_buttons)
     bot.send_message(
         chat_id=update.message.chat_id,
         text="Complete the following tasks",
